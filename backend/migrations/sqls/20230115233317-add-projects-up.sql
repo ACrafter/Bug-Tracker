886f88b7efb52b6e923c354099e5965d7c71ade5
ACrafter <ahmed2002yasser.ay@gmail.com>
2023-02-06 17:13:52 +0200

    Modified DB Tables

--- backend/migrations/sqls/20230115233317-add-projects-up.sql
+++ backend/migrations/sqls/20230115233317-add-projects-up.sql
@@ -3,5 +3,6 @@ CREATE TABLE projects(
     title VARCHAR(50) NOT NULL,
     project_lang VARCHAR(50) NOT NULL,
     department VARCHAR(50) NOT NULL,
+    rating INT NOT NULL,
     last_mod_by INT REFERENCES users(id)
 );
--- backend/migrations/sqls/20230116233322-add-tickets-up.sql
+++ backend/migrations/sqls/20230116233322-add-tickets-up.sql
@@ -3,6 +3,7 @@ CREATE TABLE tickets(
     id SERIAL PRIMARY KEY NOT NULL,
     ticket_status ticket_status_type DEFAULT 'Open' NOT NULL,
     title VARCHAR(50) NOT NULL,
+    ticket_type VARCHAR(50) NOT NULL,
     ticket_Description TEXT NOT NULL,
     last_check VARCHAR(50),
     projectid INT REFERENCES projects(id) NOT NULL
