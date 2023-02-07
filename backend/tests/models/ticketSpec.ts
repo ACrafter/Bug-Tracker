72cdb2fe0c217fe17a4f9a49997f346adb6d2ea2
ACrafter <ahmed2002yasser.ay@gmail.com>
2023-02-07 05:18:34 +0200

    Added Ticket Model

--- backend/tests/models/ticketSpec.ts
+++ backend/tests/models/ticketSpec.ts
@@ -1,56 +1,71 @@
-// import { TicketStore } from "../../src/models/ticket";
+import { ProjectStore } from "../../src/models/project";
+import { TicketStore } from "../../src/models/ticket";
 
 
-// describe("Ticket Model Tests", () => {
-//     const P = new TicketStore();
+describe("Ticket Model Tests", () => {
+    const P = new TicketStore();
 
-//     describe("CRUD Methods Existance", () => {
-//         // Default CRUD Operations
-//         it("Index Method", () => {
-//             expect(P.index).toBeDefined()
-//         })
+    describe("CRUD Methods Existance", () => {
+        // Default CRUD Operations
+        it("Index Method", () => {
+            expect(P.index).toBeDefined()
+        })
 
-//         it("Create Method", () => {
-//             expect(P.create).toBeDefined()
-//         })
+        it("Create Method", () => {
+            expect(P.create).toBeDefined()
+        })
 
-//         it("Show Method", () => {
-//             expect(P.getOne).toBeDefined()
-//         })
+        it("Show Method", () => {
+            expect(P.getOne).toBeDefined()
+        })
 
-//         it("Update Method", () => {
-//             expect(P.update).toBeDefined()
-//         })
+        it("Update Method", () => {
+            expect(P.update).toBeDefined()
+        })
 
-//         it("Delete Method", () => {
-//             expect(P.delete).toBeDefined()
-//         })
-//     })
+        it("Delete Method", () => {
+            expect(P.delete).toBeDefined()
+        })
+    })
 
-//     describe("CRUD Methods Functionality", () => {
-//         it("Index Method", async () => {
-//             const res = await P.index()
-//             expect(res).toEqual([])
-//         })
+    describe("CRUD Methods Functionality", () => {
+        let projectId: Number;
+        let ticketId: Number;
 
-//         it("Create Method", async () => {
-//             const res = await P.create({id: 1, title: "test project", project_lang: "js", project_desc:"Projectdesc",department: "TTD", rating: 4, last_mod_by: null})
-//             expect(res).toEqual({id: 1, title: "test project", project_lang: "js", project_desc:"Projectdesc", department: "TTD", rating: 4, last_mod_by: null})
-//         })
+        beforeAll(async () => {
+            const P = new ProjectStore()
+            const project = await P.create({title: "test project", project_lang: "js", project_desc:"Projectdesc",department: "TTD", rating: 4, last_mod_by: null})
+            projectId = project.id as Number
+        })
 
-//         it("Show Method", async () => {
-//             const res = await P.getOne("1")
-//             expect(res).toEqual({id: 1, title: "test project", project_lang: "js", project_desc:"Projectdesc", department: "TTD", rating: 4, last_mod_by: null})
-//         })
+        afterAll(async () => {
+            const P = new ProjectStore()
+            await P.delete(projectId)
+        })
+        it("Index Method", async () => {
+            const res = await P.index()
+            expect(res).toEqual([])
+        })
 
-//         it("Update Method", async () => {
-//             const res = await P.update("1", "rating", 6)
-//             expect(res).toEqual({id: 1, title: "test project", project_lang: "js", project_desc:"Projectdesc", department: "TTD", rating: 6, last_mod_by: null})
-//         })
+        it("Create Method", async () => {
+            const res = await P.create({title: "test ticket", ticket_type: "new feature", ticket_description:"ticketDesc", projectid:projectId})
+            ticketId = res.id as Number;
+            expect(res).toEqual({id: ticketId, title: "test ticket", ticket_type: "new feature", ticket_description:"ticketDesc", projectid:projectId, ticket_status:"Open", last_check: null})
+        })
 
-//         it("Delete Method", async () => {
-//             const res = await P.delete("1")
-//             expect(res).toEqual({id: 1, title: "test project", project_lang: "js", project_desc:"Projectdesc", department: "TTD", rating: 6, last_mod_by: null})
-//         })
-//     })
-// })
+        it("Show Method", async () => {
+            const res = await P.getOne(ticketId)
+            expect(res).toEqual({id: ticketId, title: "test ticket", ticket_type: "new feature", ticket_description:"ticketDesc", projectid:projectId, ticket_status:"Open", last_check: null})
+        })
+
+        it("Update Method", async () => {
+            const res = await P.update(ticketId, "ticket_status", "Closed")
+            expect(res).toEqual({id: ticketId, title: "test ticket", ticket_type: "new feature", ticket_description:"ticketDesc", projectid:projectId, ticket_status:"Closed", last_check: null})
+        })
+
+        it("Delete Method", async () => {
+            const res = await P.delete(ticketId)
+            expect(res).toEqual({id: ticketId, title: "test ticket", ticket_type: "new feature", ticket_description:"ticketDesc", projectid:projectId, ticket_status:"Closed", last_check: null})
+        })
+    })
+})
