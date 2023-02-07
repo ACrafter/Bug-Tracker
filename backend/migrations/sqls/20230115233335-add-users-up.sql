1bc7256e3783fa36126b167a3602f74d999d529b
ACrafter <ahmed2002yasser.ay@gmail.com>
2023-02-07 04:29:54 +0200

    Updated password size

--- backend/migrations/sqls/20230115233335-add-users-up.sql
+++ backend/migrations/sqls/20230115233335-add-users-up.sql
@@ -2,7 +2,7 @@ CREATE TYPE user_status AS ENUM ('Dev', 'Lead', 'Admin', 'Other');
 CREATE TABLE users(
     id SERIAL NOT NULL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
-    user_password VARCHAR(50) NOT NULL,
+    user_password VARCHAR(100) NOT NULL,
     user_email VARCHAR(50) NOT NULL,
     user_rank user_status DEFAULT 'Other' NOT NULL,
     department VARCHAR(50)
