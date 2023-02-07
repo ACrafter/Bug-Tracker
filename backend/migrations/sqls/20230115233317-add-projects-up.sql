32b80091da2d8d540d48b8f808d98e31a73006fd
ACrafter <ahmed2002yasser.ay@gmail.com>
2023-02-07 03:02:37 +0200

    Modified The Projects Table

--- backend/migrations/sqls/20230115233317-add-projects-up.sql
+++ backend/migrations/sqls/20230115233317-add-projects-up.sql
@@ -2,6 +2,7 @@ CREATE TABLE projects(
     id SERIAL NOT NULL PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     project_lang VARCHAR(50) NOT NULL,
+    project_desc TEXT NOT NULL,
     department VARCHAR(50) NOT NULL,
     rating INT NOT NULL,
     last_mod_by INT REFERENCES users(id)
