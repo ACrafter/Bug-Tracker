8f4beac00cfdd7c9a7eb3098ba4f3eaeb219d2d2
ACrafter <ahmed2002yasser.ay@gmail.com>
2023-02-14 18:01:47 +0200

    Added Authentication

--- backend/src/middleware/Authorization.ts
+++ backend/src/middleware/Authorization.ts
@@ -1,19 +1,34 @@
 import Express from "express";
 import { verify } from "jsonwebtoken";
+import { UserStore } from "../models/users";
 
-const Auth = (
+const U = new UserStore()
+
+export const Auth = async (
   req: Express.Request,
   res: Express.Response,
   next: Express.NextFunction
-): void => {
+): Promise<void> => {  
   try {
     const token = String(req.headers.authorization);
-    verify(token, String(process.env.TOKEN));
-    next();
+    const id = typeof req.params.id === 'undefined' ? 0 : Number(req.params.id)
+    const userId = verify(token, String(process.env.TOKEN));
+    const isAdmin = await U.isAdmin(Number(userId))
+    
+
+    if(isAdmin !== false) {
+      next();
+    } else if (id === Number(userId)){
+      next()
+    } else {
+      res.status(403);
+      res.json("Access denied, invalid token");
+    }
   } catch (err) {
     res.status(401);
-    res.json("Access denied, invalid token");
+    res.json("Access denied, no token");
   }
 };
 
-export default Auth;
+export default Auth
+
